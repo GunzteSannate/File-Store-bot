8a5ea6581dd1d57b88019d4e840069f080a8017b
GunzteSannate <ravianand361@gmail.com>
2024-09-09 00:09:49 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,11 +11,12 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {NONE}
+
 
 sɪᴢᴇ ⚙️: {file_size}
 
-Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz)</b>""" 
+Jᴏɪɴ [BACKUP CHANNEL](https://t.me/+3GeFpxpTAMozZWJl)</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
